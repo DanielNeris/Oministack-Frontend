c2fa85d3234611eabb85dd8adcc5815fe8dda223
Daniel de Souza Neri <danielneris01@gmail.com>
2019-07-16 21:46:38 -0300

    pag feed get data from api

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import api from '../../services/api';
+
 import more from '../../assets/more.svg';
 import like from '../../assets/like.svg';
 import comment from '../../assets/comment.svg';
@@ -8,64 +10,58 @@ import send from '../../assets/send.svg';
 import './index.css';
 
 class Feed extends Component {
+    state = {
+        feed: [],
+    };
+
+    componentDidMount() {
+        this.loadFeed();
+    }
+
+    loadFeed = async () => {
+        try {
+            const response = await api.get('posts');
+
+            this.setState({ feed: response.data });
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     render() {
+        const { feed } = this.state;
         return (
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Daniel</span>
-                            <span className="place">SP</span>
-                        </div>
+                { feed.map(item => (
+                    <article>
+                        <header>
+                            <div className="user-info">
+                                <span>{item.author}</span>
+                                <span className="place">{item.place}</span>
+                            </div>
 
-                        <img src={more} alt="Mais" />
-                    </header>
+                            <img src={more} alt="Mais" />
+                        </header>
 
-                    <img src="" alt="" />
+                        <img src={`http://localhost:3333/files/${item.image}`} alt="" />
 
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="" />
-                            <img src={comment} alt="" />
-                            <img src={send} alt="" />
-                        </div>
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt="" />
+                                <img src={comment} alt="" />
+                                <img src={send} alt="" />
+                            </div>
 
-                        <strong>900 curtidas</strong>
+                            <strong>{item.likes} curtidas</strong>
 
-                        <p>
-                            Um post bem loko
-                            <span>#React #node</span>
-                        </p>
-                    </footer>
-                </article>
-
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Daniel</span>
-                            <span className="place">SP</span>
-                        </div>
-
-                        <img src={more} alt="Mais" />
-                    </header>
-
-                    <img src="" alt="" />
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="" />
-                            <img src={comment} alt="" />
-                            <img src={send} alt="" />
-                        </div>
-
-                        <strong>900 curtidas</strong>
-
-                        <p>
-                            Um post bem loko
-                            <span>#React #node</span>
-                        </p>
-                    </footer>
-                </article>
+                            <p>
+                                {item.descriptions}
+                                <span>{item.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                )) }
             </section>
         );
     }
